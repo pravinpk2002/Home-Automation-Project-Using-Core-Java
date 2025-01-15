12ad25982d699786b42090ba213f591878dcc72c
Pravin Kale <121395094+pravinpk2002@users.noreply.github.com>
2025-01-15 22:15:51 +0530

    Update MainApp.java

--- src/com/houseautomation/app/MainApp.java
+++ src/com/houseautomation/app/MainApp.java
@@ -13,70 +13,11 @@ public class MainApp {
 
 	
 	public static void main(String[] args) {
-//		LocalTime time1 = LocalTime.now();
-//		
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
-////		System.out.println(time1.format(formatter));
-//		
-		LocalTime time2 = LocalTime.of(15, 30, 40);
-//		
-////		System.out.println(time2);
-//		
-//		 Duration duration = Duration.between(time1, time2);
-//		 
-//		 long hours = duration.toHours();
-//	        long minutes = duration.toMinutes() % 60;  // minutes modulo 60
-//	        long seconds = duration.getSeconds() % 60; 
-//	        
-//	        System.out.println("Time Difference: " + hours + " hours " + minutes + " minutes " + seconds + " seconds");
-		
-		
-//		Television tv = new Television("samsung", "SM7890tv");
-//		
-//		tv.checkStatus();
-//		tv.setState(true);
-//		tv.setPreviousStateTime(time2);
-//		System.out.println("******************************");
-//		tv.checkStatus();
-//		
-//		
-//		System.out.println("******************************");
-//		MusicPlayer mp = new MusicPlayer("sony", "sn908u");
-//		
-//		mp.checkStatus();
-//		mp.setState(true);
-//		mp.setPreviousStateTime(time2);
-//		System.out.println("******************************");
-//		mp.checkStatus();
-//		
-//		
-//		
-//		LED led = new LED("philips", "ph7898b");
-//		System.out.println("******************************");
-//		led.checkStatus();
-//		led.setBrightness(4);
-//		led.setState(true);
-//		led.setPreviousStateTime(time2);
-//		System.out.println("******************************");
-//		led.checkStatus();
-//		
-//		System.out.println("******************************");
-//		
-//		Chandler chd = new Chandler("wipro", "whj700");
-//		chd.checkStatus();
-//		System.out.println("******************************");
-//		chd.setState(true);
-//		chd.checkStatus();
-//		System.out.println("******************************");
-//		chd.changeColor();
-//		chd.changeColor();
-////		chd.changeColor();
-//		chd.checkStatus();
-		
+
+
 		
 		HomeAutomationService service = new HomeAutomationService();
-//		
-//		
+	
 		service.start();
 		
 		
